acb81c122b648cef9afa4cbc347b9b4e5a0c32f9
D3CR1PT3R <47520842+vicken999@users.noreply.github.com>
2020-11-13 13:52:14 +0200

    first commit

---
+++ BankingApp/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Banking;
+
+namespace BankingApp
+{
+    public partial class MainPage : ContentPage
+    {
+        BankAccount _account; 
+        public MainPage()
+        {
+            InitializeComponent();
+            Bank fnb = new Bank("First National Bank", 4324, "Kenilworth");
+            Customer myNewCustomer = new Customer("7766445424", "10 me at home", "Bob", "The Builder");
+            fnb.AddCustomer(myNewCustomer);
+
+            _account = myNewCustomer.ApplyForBankAccount();
+        }
+
+        private void DepositButton_Clicked(object sender, EventArgs e)
+        {
+            decimal depositAmount = 0;
+            var valid = decimal.TryParse(DepositAmountEntry.Text, out depositAmount);
+           
+
+            if (valid)
+            {
+                _account.DepositMoney(depositAmount, DateTime.Now, "Stipend");
+            }
+            else
+            {
+                DisplayAlert("Validation Error", "Please Enter Amount", "Cancel");
+            }
+        }
+    }
+}
---
+++ BankingApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Banking;
+
+namespace Banks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank fnb = new Bank("First National Bank", 4324, "Kenilworth");
+            Customer myNewCustomer = new Customer("7766445424", "10 me at home", "Bob", "The Builder");
+            fnb.AddCustomer(myNewCustomer);
+
+            var account = myNewCustomer.ApplyForBankAccount();
+            account.DepositMoney(1500, DateTime.Now, "Stipend");
+
+            try
+            {
+                account.WithdrawMoney(500, DateTime.Now, "Buy Lobster");
+                account.WithdrawMoney(500, DateTime.Now, "Buy Schnitzel");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message + " go speak to Wouter.");
+            }
+
+            Console.WriteLine("Your Balance is:" + account.Balance);
+
+            var history = account.GetTransactionHistory();
+
+            Console.WriteLine(history);
+        }
+    }
+}
